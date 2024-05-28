54c81f7c996985130f191f92d0f44e21cd1be5c2
Alvaro Condori <con21033@byui.edu>
2024-05-28 00:28:22 -0300

    connect to mongodb with mongoose

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const mongoose = require("mongoose");
+
+// CONNECT DATABASE
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hi from Node js");
