e2b914bce0f67c2372b3c0e15e09098f8e8230dc
Alvaro Condori <con21033@byui.edu>
2024-05-28 21:44:53 -0300

    .

--- app.js
+++ app.js
@@ -23,6 +23,18 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
 // ROUTES MIDDLEWARE
 app.use("/", routes);
